1afdd5468411aefd7e931ec41cc3e1e90e05ac16
IngaPanczyk <ingawaaa@gmail.com>
2021-03-09 18:39:14 +0100

    odkomentowanie

--- src/main/java/com/kodilla/books/MainView.java
+++ src/main/java/com/kodilla/books/MainView.java
@@ -1,6 +1,6 @@
 package com.kodilla.books;
 
-/*import com.kodilla.books.domain.Book;
+import com.kodilla.books.domain.Book;
 import com.kodilla.books.grafs.Graf;
 import com.kodilla.books.service.BookService;
 import com.vaadin.flow.component.button.Button;
@@ -10,13 +10,13 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
-import com.vaadin.flow.router.Route;*/
+import com.vaadin.flow.router.Route;
 
 
-//@Route
-public class MainView /*extends VerticalLayout*/ {
+@Route
+public class MainView extends VerticalLayout {
 
-   /* Graf graf = new Graf();
+    Graf graf = new Graf();
 
     private BookService bookService = BookService.getInstance();
     private Grid grid = new Grid<>(Book.class);
@@ -26,12 +26,12 @@ public class MainView /*extends VerticalLayout*/ {
     private TextField filter = new TextField();
 
     //Formularz
-    private BookForm form = new BookForm(this);
+  //  private BookForm form = new BookForm(this);
 
     //Nowe pozycje
     private Button addNewBook = new Button("Add new book");
 
-    public MainView(*//*Graf drowGraf*//*) {
+    public MainView(/*Graf drowGraf*/) {
 
         //this.graf = drowGraf;
 
@@ -43,21 +43,21 @@ public class MainView /*extends VerticalLayout*/ {
 
         addNewBook.addClickListener(e -> {
             grid.asSingleSelect().clear();
-            form.setBook(new Book());
+          //  form.setBook(new Book());
         });
 
         HorizontalLayout toolbar = new HorizontalLayout(filter, addNewBook,image);
 
-        HorizontalLayout mainContent = new HorizontalLayout(grid, form);
-        mainContent.setSizeFull();
-        grid.setSizeFull();
+     //  HorizontalLayout mainContent = new HorizontalLayout(grid, form);
+      //  mainContent.setSizeFull();
+     //   grid.setSizeFull();
 
-        add(toolbar, mainContent);
-        form.setBook(null);
+     //   add(toolbar, mainContent);
+     //   form.setBook(null);
         setSizeFull();
         refresh();
 
-        grid.asSingleSelect().addValueChangeListener(event -> form.setBook((Book) grid.asSingleSelect().getValue()));
+       // grid.asSingleSelect().addValueChangeListener(event -> form.setBook((Book) grid.asSingleSelect().getValue()));
     }
 
     public void refresh() {
@@ -66,6 +66,6 @@ public class MainView /*extends VerticalLayout*/ {
 
     private void update() {
         grid.setItems(bookService.findByTitle(filter.getValue()));
-    }*/
+    }
 
 }
